6de395da1d7bbc2c57c0f9bbb81538ee5a08244c
Felipe-Rincon <afeliperinconte@gmail.com>
2025-07-10 22:27:58 -0500

    Descripción de los cambios que estás subiendo

--- Procfile
+++ Procfile
@@ -1,3 +1,5 @@
-web: streamlit run app.py --server.port=$PORT
+web: uvicorn app:app --host=0.0.0.0 --port=$PORT
+
+#web: streamlit run app.py --server.port=$PORT
 
 #web: python app_fastapi.py
---
+++ app_streamlit.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+if "authentication_status" not in st.session_state:
+    hide_sidebar_css = """
+    <style>
+        section[data-testid="stSidebar"] {
+            display: none !important;
+        }
+        button[title="Toggle sidebar"] {
+            display: none !important;
+        }
+    </style>
+    """
+    st.markdown(hide_sidebar_css, unsafe_allow_html=True)
+
+
+def login():
+    st.session_state["authentication_status"] = True
+
+def logout():
+    st.session_state["authentication_status"] = None
+
+if "authentication_status" not in st.session_state:
+    st.session_state["authentication_status"] = None
+
+if st.session_state["authentication_status"] is None:
+
+    st.title("Prueba Tecnica")
+    if st.button("Log in"):
+        login()
+        st.rerun()
+
+if st.session_state["authentication_status"]:
+
+
+    menu_page = st.Page("pages_app/0_menu.py", title="Menú", icon=":material/menu:")
+    part_1 = st.Page("pages_app/1_part_1.py", title="Parte 1", icon=":material/app_registration:")
+    
+    part_2_scenario_1 = st.Page("pages_app/2_part_2_scenario_1.py", title="Parte 2 - 1", icon=":material/app_registration:")
+    part_2_scenario_2 = st.Page("pages_app/3_part_2_scenario_2.py", title="Parte 2 - 2", icon=":material/app_registration:")
+    
+    part_3 = st.Page("pages_app/4_part_3.py", title="Parte 3", icon=":material/app_registration:")
+
+    pg = st.navigation(
+        {
+            "Menú": [menu_page],
+            "Parte_1": [part_1],
+            "Parte_2": [part_2_scenario_1, part_2_scenario_2],
+            "Parte_3": [part_3]
+
+        }
+    )
+
+    pg.run()
+
+    if st.button("Log out"):
+        logout()
+        st.rerun()
